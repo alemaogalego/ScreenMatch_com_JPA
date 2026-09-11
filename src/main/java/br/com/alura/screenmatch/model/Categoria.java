2103b7dc9fc8cd6804d91127e5a42ce5893d9138
Lucas César Lorena <lucascesar.lorena@gmail.com>
2026-09-11 01:34:00 -0300

    Adicionando Serie, Categoria e dando update na classe DadosSerie ; criando menu opcional na classe Principal

---
+++ src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -0,0 +1,30 @@
+package br.com.alura.screenmatch.model;
+
+public enum Categoria {
+    ACAO("Action"),
+    ROMANCE("Romance"),
+    COMEDIA("Comedy"),
+    CRIME("Crime"),
+    DRAMA("Drama"),
+    FANTASIA("Fantasy"),
+    FICCAO_CIENTIFICA("Sci-Fi"),
+    TERROR("Horror"),
+    ANIMACAO("Animation"),
+    AVENTURA("Adventure"),
+    DOCUMENTARIO("Documentary");
+
+    private String categoriaOmdb;
+
+    Categoria(String categoriaOmdb) {
+        this.categoriaOmdb = categoriaOmdb;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+}
